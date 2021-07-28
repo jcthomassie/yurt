07eaacf6d2b8f6eb27e50638a0cae19ed08b099d
Julian Thomassie <julianthomassie@gmail.com>
2021-07-27 22:32:49 -0700

    Outline clap arg parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::{crate_version, App, AppSettings};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("dots")
+        .version(crate_version!())
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .subcommand(App::new("install").about("install dotfiles"))
+        .subcommand(App::new("uninstall").about("uninstall dotfiles"))
+        .get_matches();
+
+    match matches.subcommand_name() {
+        Some("install") => {
+            println!("Installing dotfiles...");
+        }
+        Some("uninstall") => {
+            println!("Unstalling dotfiles...");
+        }
+        _ => unreachable!(),
+    }
 }
