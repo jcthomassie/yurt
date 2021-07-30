dd80f2edfdc1b2ee9bd09b7b47c76e2d6c9c22d1
Julian Thomassie <thomajl@amazon.com>
2021-07-30 14:09:32 -0700

    Add install --clean subcommand

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,17 @@ use clap::{crate_authors, crate_version, App, AppSettings, Arg, ArgMatches};
 use error::DotsResult;
 
 #[inline(always)]
-fn parse_build(matches: ArgMatches) -> DotsResult<yaml::Build> {
+fn parse_build(matches: &ArgMatches) -> DotsResult<yaml::Build> {
     yaml::parse(link::expand_path(matches.value_of("yaml").unwrap())?)
 }
 
 #[inline(always)]
-fn parse_resolve_build(matches: ArgMatches) -> DotsResult<(repo::Repo, Vec<link::Link>)> {
+fn parse_resolve_build(matches: &ArgMatches) -> DotsResult<(repo::Repo, Vec<link::Link>)> {
     parse_build(matches)?.resolve()
 }
 
 fn show(matches: ArgMatches) -> DotsResult<()> {
-    let build = parse_build(matches)?;
+    let build = parse_build(&matches)?;
     let (_, links) = build.resolve()?;
     println!("Locale:\n{:#?}", *yaml::LOCALE);
     println!("_______________________________________");
@@ -29,7 +29,12 @@ fn show(matches: ArgMatches) -> DotsResult<()> {
 }
 
 fn install(matches: ArgMatches) -> DotsResult<()> {
-    let (repo, links) = parse_resolve_build(matches)?;
+    let (repo, links) = parse_resolve_build(&matches)?;
+    // Optionally clean before install
+    let sub = matches.subcommand_matches("install").unwrap();
+    if sub.is_present("clean") {
+        clean(matches)?;
+    }
     println!("Installing dotfiles...");
     repo.require()?;
     link::install_links(links)?;
@@ -38,14 +43,14 @@ fn install(matches: ArgMatches) -> DotsResult<()> {
 }
 
 fn uninstall(matches: ArgMatches) -> DotsResult<()> {
-    let (_, links) = parse_resolve_build(matches)?;
+    let (_, links) = parse_resolve_build(&matches)?;
     println!("Unstalling dotfiles...");
     link::uninstall_links(links)?;
     Ok(())
 }
 
 fn clean(matches: ArgMatches) -> DotsResult<()> {
-    let (_, links) = parse_resolve_build(matches)?;
+    let (_, links) = parse_resolve_build(&matches)?;
     println!("Cleaning invalid links...");
     link::clean_links(links)?;
     Ok(())
@@ -62,7 +67,15 @@ fn main() -> DotsResult<()> {
         .version(crate_version!())
         .about("Simple CLI tool for dotfile management.")
         .setting(AppSettings::SubcommandRequiredElseHelp)
-        .subcommand(App::new("install").about("Installs dotfiles"))
+        .subcommand(
+            App::new("install").about("Installs dotfiles").arg(
+                Arg::new("clean")
+                    .about("Run `dots clean` before install")
+                    .short('c')
+                    .long("clean")
+                    .takes_value(false),
+            ),
+        )
         .subcommand(App::new("uninstall").about("Uninstalls dotfiles"))
         .subcommand(App::new("clean").about("Cleans output destinations"))
         .subcommand(App::new("update").about("Updates dotfiles and/or system"))
