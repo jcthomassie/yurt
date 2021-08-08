99b4ca915d6ff5e1e8a63daac11cc6e009fd7a8b
Julian Thomassie <thomajl@amazon.com>
2021-08-08 11:51:20 -0700

    Remove unused source struct

--- src/pack.rs
+++ src/pack.rs
@@ -1,12 +1,10 @@
 use anyhow::{anyhow, Result};
 use lazy_static::lazy_static;
 use log::{debug, info, warn};
-use regex::Regex;
 use serde::Deserialize;
 use std::borrow::Cow;
 use std::env;
-use std::fs::File;
-use std::io::{BufRead, BufReader, Read, Write};
+use std::io::{Read, Write};
 use std::process::{Child, Command, Output, Stdio};
 
 pub use PackageManager::{Apt, AptGet, Brew, Cargo, Choco, Yum};
@@ -14,43 +12,6 @@ pub use Shell::{Bash, Powershell, Sh, Zsh};
 
 lazy_static! {
     static ref SHELL: Shell<'static> = Shell::from_env();
-}
-
-#[derive(Debug, PartialEq, Deserialize)]
-pub struct Source(Vec<String>);
-
-#[allow(dead_code)]
-impl Source {
-    // Resolve source file paths
-    pub fn resolve(&self) -> Result<Self> {
-        let mut vec: Vec<String> = Vec::with_capacity(self.0.len());
-        for path in &self.0 {
-            vec.push(shellexpand::full(path)?.to_string())
-        }
-        Ok(Source(vec))
-    }
-
-    // Reload all of the source files
-    pub fn reload(&self) -> Result<()> {
-        let re = Regex::new(r#"export ([0-9A-Za-z_-]+)=([0-9A-Za-z_"-:{}\$]+)[;$]?"#).unwrap();
-        for path in &self.0 {
-            let file = File::open(path)?;
-            for line in BufReader::new(file).lines() {
-                for cap in re.captures_iter(&line?) {
-                    let k = &cap[1];
-                    let v = shellexpand::full(&cap[2]);
-                    match v {
-                        Ok(v) => {
-                            debug!("Setting ${} = {}", k, v);
-                            env::set_var(k, v.as_ref());
-                        }
-                        Err(e) => warn!("Failed setting ${}; {:?}", k, e),
-                    };
-                }
-            }
-        }
-        Ok(())
-    }
 }
 
 trait Cmd {
